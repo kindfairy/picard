78825b0f1e0188fbf194f7ef402903923f5a3239
kindfairy <henrygale4815@gmail.com>
2017-04-21 21:45:22 +0300

    Now, it works

--- src/jmh/java/picard/analysis/SimpleBenchmark.java
+++ src/jmh/java/picard/analysis/SimpleBenchmark.java
@@ -35,46 +35,42 @@ public class SimpleBenchmark {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @OutputTimeUnit(TimeUnit.SECONDS)
     @Fork(1)
     @Warmup(iterations = 0)
     @Measurement(iterations = 1)
     public void measureGC() {
         String[] args=new String[5];
         StringBuilder sb=new StringBuilder();
-        StringTokenizer stringTokenizer=new StringTokenizer("I=C:\\Users\\ABRA\\tmp.picard\\NA12878.chrom11.ILLUMINA.bwa.CEU.exome.20121211.bam\n" +
-                "O=C:\\Users\\ABRA\\tmp.picard\\out_CollectGcBiasMetrics.txt\n" +
-                "CHART=C:\\Users\\ABRA\\tmp.picard\\gc_bias_metrics.pdf\n" +
-                "S=C:\\Users\\ABRA\\tmp.picard\\summary_metrics.txt\n" +
-                "R=C:\\Users\\ABRA\\tmp.picard\\hs37d5-002.fa");
+        StringTokenizer stringTokenizer=new StringTokenizer("CHART=/home/henry/Documents/epam-practice/test/gc_bias_metrics.pdf \n" +
+                "S=/home/henry/Documents/epam-practice/test/out-summary_metrics.txt \n" +
+                "I=/home/henry/Documents/epam-practice/test/NA12878.chrom11.ILLUMINA.bwa.CEU.exome.20121211.bam \n" +
+                "O=/home/henry/Documents/epam-practice/test/out-gc_bias_metrics.txt \n" +
+                "R=/home/henry/Documents/epam-practice/test/hs37d5.fa");
         for(int i=0;stringTokenizer.hasMoreTokens();i++){
             args[i]=stringTokenizer.nextToken();
         }
 
-        CollectGcBiasMetrics.main(args);
+        new CollectGcBiasMetrics().instanceMain(args);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @OutputTimeUnit(TimeUnit.SECONDS)
     @Fork(1)
     @Warmup(iterations = 0)
     @Measurement(iterations = 1)
     public void measureCISM() {
-        String[] args=new String[7];
+        String[] args=new String[3];
         StringBuilder sb=new StringBuilder();
-        StringTokenizer stringTokenizer=new StringTokenizer("I=C:\\Users\\ABRA\\tmp.picard\\NA12878.chrom11.ILLUMINA.bwa.CEU.exome.20121211.bam\n" +
-                "HISTOGRAM_FILE=C:\\Users\\ABRA\\tmp.picard\\out_CollectInsertSizeMetrics_hist.txt\n" +
-                "O=C:\\Users\\ABRA\\tmp.picard\\out_CollectInsertSizeMetrics.txt\n" +
-                "R=C:\\Users\\ABRA\\tmp.picard\\hs37d5-002.fa\n" +
-                "VALIDATION_STRINGENCY=LENIENT\n" +
-                "ASSUME_SORTED=TRUE\n" +
-                "STOP_AFTER=0");
+        StringTokenizer stringTokenizer=new StringTokenizer("H=/home/henry/Documents/epam-practice/test/insert_size_histogram.pdf \n" +
+                "I=/home/henry/Documents/epam-practice/test/NA12878.chrom11.ILLUMINA.bwa.CEU.exome.20121211.bam \n" +
+                "O=/home/henry/Documents/epam-practice/test/out-insert_size_metrics.txt ");
         for(int i=0;stringTokenizer.hasMoreTokens();i++){
             args[i]=stringTokenizer.nextToken();
         }
 
-        CollectInsertSizeMetrics.main(args);
+        new CollectInsertSizeMetrics().instanceMain(args);
 
     }
 
